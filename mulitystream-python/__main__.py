03545106bd9d4f003d25742fe1967448aadb40af
NicholasBlackburn1 <nickblackburn02@gmail.com>
2022-02-22 12:15:26 -0500

    working on basic stuff

---
+++ mulitystream-python/__main__.py
@@ -0,0 +1,21 @@
+"""
+this is the main class this is where the pram run  fom
+"""
+
+
+from utils import consoleLog
+
+
+# starts the main program
+def main():
+    consoleLog.info("starting the Stream server")
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ mulitystream-python/stream/inputstream.py
@@ -0,0 +1,15 @@
+
+"""
+
+this file is for handling the input stream from obs uwu
+"""
+
+
+
+
+class inputstream(object):
+
+
+
+    def init():
+        pass
